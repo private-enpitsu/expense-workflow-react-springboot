1c7c58de228655922cd391b92c4926ecdddbfad7
tomo <ccc@gmail.com>
2026-01-27 14:41:01 +0900

    共有ファイルを追加

---
+++ 共有ファイル/共有-開発資料/app/expense-workflow-react-springboot/backend/expense-workflow-backend/src/main/java/com/example/expenseworkflow/ExpenseWorkflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.expenseworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseWorkflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseWorkflowBackendApplication.class, args);
+	}
+
+}
---
+++ 共有ファイル/共有-開発資料/app/expense-workflow-react-springboot/backend/expense-workflow-backend/src/test/java/com/example/expenseworkflow/ExpenseWorkflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expenseworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseWorkflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
